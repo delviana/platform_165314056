de83452293213d10058fcd8bd34a27e4ae9c7775
basisa03 <basisa03@BASIS-A03.basisdata.ti.usd>
2018-10-30 16:06:27 +0700

    platform 0.4

--- NetBeansProjects/rsku165314056/src/java/Helper/pasienHelper.java
+++ NetBeansProjects/rsku165314056/src/java/Helper/pasienHelper.java
@@ -7,16 +7,19 @@ package Helper;
 
 import Pojos.Pasien;
 import Util.rskuHibernateUtil;
+import java.util.Date;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author basisa03
  */
 public class pasienHelper {
-      public List<Pasien> getAllPasien() {
+
+    public List<Pasien> getAllPasien() {
         List<Pasien> result = null;
         Session session = rskuHibernateUtil.getSessionFactory().openSession();
         String query = "from Pasien p";
@@ -24,4 +27,21 @@ public class pasienHelper {
         result = q.list();
         session.close();
         return result;
-}}
+    }
+
+    public void addNewPasien(
+            String noRm,
+            String nama,
+            String alamat,
+            String nik,
+            Date tanggalLahir,
+            String kelamin) {
+        Session session = rskuHibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Pasien pasien = new Pasien(noRm, nama, alamat, noRm, tanggalLahir, kelamin);
+        session.saveOrUpdate(pasien);
+        transaction.commit();
+        session.close();
+    }
+
+}
--- NetBeansProjects/rsku165314056/src/java/Test/TestPasienAddNew.java
+++ NetBeansProjects/rsku165314056/src/java/Test/TestPasienAddNew.java
@@ -5,10 +5,33 @@
  */
 package Test;
 
+import Helper.pasienHelper;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author basisa03
  */
 public class TestPasienAddNew {
-    
+
+    public static void main(String[] args) {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+            String tglLahir = "19741231";
+            Date date = format.parse(tglLahir);
+            String noRm = "654321";
+            String nama = "Adi";
+
+            pasienHelper helper = new pasienHelper();
+            String alamat = "paingan";
+            String kelamin = "lelaki";
+            helper.addNewPasien(noRm, nama, alamat, noRm, date, kelamin);
+        } catch (ParseException ex) {
+            Logger.getLogger(TestPasienAddNew.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
